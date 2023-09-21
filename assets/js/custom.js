ececcc462753903d523de87247eff09370df6802
XelgaAr <143228153+XelgaAr@users.noreply.github.com>
2023-09-21 21:06:23 +0300

    first

---
+++ assets/js/custom.js
@@ -0,0 +1,55 @@
+  const burger = document.querySelector('#hamburger');
+  burger.addEventListener('click', function() {
+      burger.classList.toggle('is-active');
+  });
+  document.getElementById('fileInput').onchange = function () {
+    
+
+// document.getElementById('file-name').innerHTML = this.files[0].name;
+
+
+    document.getElementById('file-name').innerHTML = this.value;
+  };
+
+  $(document).ready(function() {
+      $('.first-screen').slick({
+          infinite: true,
+          dots: true,
+          appendArrows: '.first-screen-nav',
+          responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+            arrows: false,
+              }
+            }
+          ]
+      });
+      $('.first-screen').append($('.first-screen-nav'));
+  })
+
+  mobileOnlySlider("#advantages-slider", true, false, 992);
+
+  function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+      var slider = $($slidername);
+      var settings = {
+          mobileFirst: true,
+          dots: $dots,
+          arrows: $arrows,
+          responsive: [{
+              breakpoint: $breakpoint,
+              settings: "unslick"
+          }]
+      };
+
+      slider.slick(settings);
+
+      $(window).on("resize", function() {
+          if ($(window).width() > $breakpoint) {
+              return;
+          }
+          if (!slider.hasClass("slick-initialized")) {
+              return slider.slick(settings);
+          }
+      });
+  } // Mobile Only Slider
